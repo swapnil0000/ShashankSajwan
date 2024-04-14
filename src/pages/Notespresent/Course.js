eef50a6b719ae3a92aece65547474de2b9c0e711
Swapnil Srivastava <swapnilsrivastava1999@gmail.com>
2024-04-14 19:52:01 +0530

    notes

--- src/pages/Notespresent/Course.js
+++ src/pages/Notespresent/Course.js
@@ -46,13 +46,13 @@ const Course = () => {
   const isSm = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',justifyContent:'space-between', width: '100%', marginTop: '50px' }}>
+    <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '60%', marginTop: '50px' }}>
       <Typography sx={{ fontWeight: '700', fontSize: isXs ? '25px' : '40px', marginBottom: '10px' }}>NOTES PROVIDED</Typography>
      
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={1} justifyContent="center">
         {data.map((note) => (
           <Grid item xs={isXs ? 12 : (isSm ? 6 : 4)} key={note.id}>
-          <Box sx={{width:'290px',height:'290px',width:'100%'}}>
+          <Box sx={{width:'240px',height:'240px',width:'100%'}}>
              <Link to={note.path}>          
              <img style={{ width: '100%', height: '100%', objectFit: 'contain' }} src={note.img}  />
              </Link>
