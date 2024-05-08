d38d73ddc2b242f0534fdd109b6c8b186afea0c3
Swapnil Srivastava <swapnilsrivastava1999@gmail.com>
2024-05-08 12:15:13 +0530

    popup

--- src/components/popup/PopupDailog.js
+++ src/components/popup/PopupDailog.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "../popup/popupStyle.css";
 import popupImage from "../popup/popup.webp";
 import { useMediaQuery, useTheme } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const PopupDialog = () => {
   const [showPopup, setShowPopup] = useState(true);
@@ -14,13 +15,20 @@ const PopupDialog = () => {
     setShowPopup(false);
   };
 
+  const navigate=useNavigate();
+
+  const handleClick=()=>{
+    navigate('/courses');
+    setShowPopup(false);
+  }
+
   return (
     <div>
       {showPopup && (
         <div className="home-popup">
           <div className="popup">
             <div className="popup-content">
-              <img src="/assets/popup.webp" alt="Popup Image" style={{width:isMobileView?"360px":"700px",height:isMobileView?"230px":"400px"}}/>
+              <img onClick={handleClick} src="/assets/popup.webp" alt="Popup Image" style={{width:isMobileView?"360px":"700px",height:isMobileView?"230px":"400px"}}/>
               <button className="close-popup-btn" onClick={handleClose}>Close</button>
             </div>
           </div>
