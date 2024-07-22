5981fa6abcae231c1c7c01a1412797d49bda9a48
swapnil0000 <SwapnilScrrum@example.com>
2024-07-22 15:51:50 +0530

    banneradded

--- src/components/Carousels.js
+++ src/components/Carousels.js
@@ -7,10 +7,10 @@ const Carousels = () => {
   const isMobileView = useMediaQuery(theme.breakpoints.down('sm'))
 
 
-  const data=[
+  const data = [
     {
-      id:1,
-      img:"/assets/banners/banner1.png"
+      id: 1,
+      img: "/assets/banners/banner1.png"
     },
     {
       id:2,
@@ -24,39 +24,39 @@ const Carousels = () => {
       id:4,
       img:"/assets/banners/banner3.webp"
     }
-  
+
 
   ]
   return (
 
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
-      <Box sx={{ width: '100%' }}>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style={{ maxHeight: '900px', marginTop: '100px' }}>
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-          </div>      
-          <div class="carousel-inner" style={{ display: 'flex', maxWidth: '100%' }}>
-  {data.map((ele, index) => (
-    <div key={ele.id} class={`carousel-item ${index === 0 ? 'active' : ''}`}>
-      <img style={{ width: '100%', height: isMobileView ? "220px" : "480px" }} src={ele.img} alt="..." />
-    </div>
-  ))}
-</div>
-
-          <button style={{ color: 'red', fontSize: '100px' }} className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true" style={{ color: 'red', fontSize: '100px' }}></span>
-            <span className="visually-hidden" style={{ color: 'red', fontSize: '100px' }}>Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <Box sx={{ width: '100%' }}>
+      <div id="carouselExampleIndicators" className="carousel slide" style={{ maxHeight: '900px', marginTop: '100px' }}>
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
-      </Box>
+        <div className="carousel-inner" style={{ display: 'flex', maxWidth: '100%' }}>
+          {data.map((ele, index) => (
+            <div key={ele.id} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+              <img style={{ width: '100%', height: isMobileView ? "220px" : "480px" }} src={ele.img} alt="..." />
+            </div>
+          ))}
+        </div>
+
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </Box>
+  </Box>
 
   );
 }
