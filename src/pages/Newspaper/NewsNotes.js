aa9a19eeb33c7cf097bb194e665318050b9fdebf
swapnil0000 <SwapnilScrrum@example.com>
2024-07-30 16:08:54 +0530

    25

--- src/pages/Newspaper/NewsNotes.js
+++ src/pages/Newspaper/NewsNotes.js
@@ -157,7 +157,7 @@ const julyData=[
   {id:27, img:'/assets/newspaper/july/july27.webp',link:"https://drive.google.com/file/d/12V5MGsZ923QpPfuosGn_G7rAt7b8-D4O/view?usp=sharing"},
   {id:28, img:'/assets/newspaper/july/july28.webp',link:"https://drive.google.com/file/d/19_M-U13CPnh11R9R4jbx1wR2l3hP57UU/view?usp=sharing"},
   {id:29, img:'/assets/newspaper/july/july29.webp',link:"https://drive.google.com/file/d/1M9oABa8DVSwB7IFzpl-Oo2uYexo5y2hs/view"},
-  // {id:30, img:'/assets/newspaper/july/july30.webp',link:"https://drive.google.com/file/d/1YjnxL3m32_8k-fMk_FZi4T96DRzUvQqq/view?usp=sharing"},
+  {id:30, img:'/assets/newspaper/july/july30.webp',link:"https://drive.google.com/file/d/15Xk3IbMiK2RZzOZK1GQIVXkeuPdFdrI2/view"},
   // {id:31, img:'/assets/newspaper/july/july31.webp',link:"https://drive.google.com/file/d/1YjnxL3m32_8k-fMk_FZi4T96DRzUvQqq/view?usp=sharing"},
 
 
