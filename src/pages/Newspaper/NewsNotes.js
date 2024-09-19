1f8ade3428b97ed0648b71eba812338d4d99f39c
swapnil0000 <SwapnilScrrum@example.com>
2024-09-19 17:26:39 +0530

    19sep

--- src/pages/Newspaper/NewsNotes.js
+++ src/pages/Newspaper/NewsNotes.js
@@ -215,6 +215,7 @@ const sepData=[
   {id:15,img:'/assets/newspaper/sep/16sep.jpg',link:'https://drive.google.com/file/d/1NiSEu9-20FdKlr6zFsibPaHnKFZlLa02/view?usp=sharing'},
   {id:16,img:'/assets/newspaper/sep/17sep.jpg',link:'https://drive.google.com/file/d/12Duz4hVRQ72xa7C1LxNmHtWddMPlQY-R/view?usp=sharing'},
   {id:16,img:'/assets/newspaper/sep/18sep.jpg',link:'https://drive.google.com/file/d/1mgWUkqSvfSjgA8hxNQ_ZxFrIeUwVOIRn/view?usp=sharing'},
+  {id:16,img:'/assets/newspaper/sep/19sep.jpg',link:'https://drive.google.com/file/d/1IewxlWWxtEg_aufv7Nc7kxLt_7SJWjR4/view?usp=sharing'},
   // {id:1,img:'/assets/newspaper/sep/1sep.jpg',link:''},
   // {id:1,img:'/assets/newspaper/sep/1sep.jpg',link:''},
   // {id:1,img:'/assets/newspaper/sep/1sep.jpg',link:''},
