85a9698f7950193d7c1773db78a3597ec3f9b4b9
swapnil0000 <SwapnilScrrum@example.com>
2024-10-17 20:51:05 +0530

    17oct

--- src/pages/Newspaper/NewsNotes.js
+++ src/pages/Newspaper/NewsNotes.js
@@ -257,6 +257,7 @@ const octData = [
   {id:14,img:'/assets/newspaper/oct/14oct.jpeg',link:'https://drive.google.com/file/d/1KMy_TSUF4u-7cIyNkDtHOv08LGs3s-Os/view?usp=sharing'},
   {id:15,img:'/assets/newspaper/oct/15oct.jpg',link:'https://drive.google.com/file/d/1BPck1ssH1lIBLvAmvXVekarcLqX76ARB/view?usp=sharing'},
   {id:16,img:'/assets/newspaper/oct/16oct.jpg',link:'https://drive.google.com/file/d/1LwCKI2Bf92wkdCJvLyaGaE3uRrRTuK_a/view?usp=sharing'},
+  {id:17,img:'/assets/newspaper/oct/17oct.jpeg',link:'https://drive.google.com/file/d/1XqQz_Ta4JyBTCU8ecXyq6bTcNq5Ca1p5/view?usp=sharing'},
   // {id:1,img:'/assets/newspaper/oct/1oct.jpg',link:''},
   // {id:1,img:'/assets/newspaper/oct/1oct.jpg',link:''},
   // {id:1,img:'/assets/newspaper/oct/1oct.jpg',link:''},
