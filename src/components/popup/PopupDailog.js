c5e0f149a311993e7987e3ce461e8b3eeb060a22
swapnil0000 <SwapnilScrrum@example.com>
2025-03-11 12:23:26 +0530

    Main Banner - Popup Updated

--- src/components/popup/PopupDailog.js
+++ src/components/popup/PopupDailog.js
@@ -6,18 +6,18 @@ import { useNavigate } from 'react-router-dom';
 
 const PopupDialog = () => {
   const [showPopup, setShowPopup] = useState(true);
-  const theme=useTheme();
-  const isMobileView=useMediaQuery(theme.breakpoints.down("md"))
+  const theme = useTheme();
+  const isMobileView = useMediaQuery(theme.breakpoints.down("md"))
   console.log("SWAPNIL")
   const handleClose = () => {
-   
+
     // Close the popup
     setShowPopup(false);
   };
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
-  const handleClick=()=>{
+  const handleClick = () => {
     navigate('/courses');
     setShowPopup(false);
   }
@@ -27,11 +27,11 @@ const PopupDialog = () => {
       {showPopup && (
         <div className="home-popup">
           <div className="popup">
-          <img onClick={handleClick} src="/assets/popup2026.jpeg" alt="Popup Image" style={{width:isMobileView?"350px":"90%",height:isMobileView?"220px":"400px",borderRadius:'5px',cursor:'pointer'}}/>
-          {!isMobileView &&<button style={{}} className="close-popup-btn" onClick={handleClose}>Close</button>}
-{        
-  isMobileView &&  <button style={{fontSize:'20px'}} className="close-popup-mobile" onClick={handleClose}>Close</button>
-}
+            <img onClick={handleClick} src="/assets/newpopup2026.jpeg" alt="Popup Image" style={{ width: isMobileView ? "350px" : "90%", height: isMobileView ? "220px" : "400px", borderRadius: '5px', cursor: 'pointer' }} />
+            {!isMobileView && <button style={{}} className="close-popup-btn" onClick={handleClose}>Close</button>}
+            {
+              isMobileView && <button style={{ fontSize: '20px' }} className="close-popup-mobile" onClick={handleClose}>Close</button>
+            }
           </div>
         </div>
       )}
